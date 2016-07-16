df5262fe841ed64c3b20df6dcab501a08e1838d5
VincentMardon <mardon.vincent@gmail.com>
2016-07-16 15:48:02 -0400

    Adds some other routes and fixes some other issues.

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,18 @@ var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
 
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
+  
+  query.exec(function(err, post){
+    if(err){ return next(err) }
+    if(!post) { return next(new Error('can\'t find post')); }
+    
+    req.post = post;
+    return next();
+  });
+});
+
 /* GET home page. */
 router.get('/posts', function(req, res, next) {
   Post.find(function(err, posts){
@@ -13,5 +25,19 @@ router.get('/posts', function(req, res, next) {
     res.json(posts);
   });
 });
+
+
+/* Create a new post */
+router.post('/posts', function(req, res, next) {
+  var post = new Post(req.body);
+  
+  post.save(function(err, post){
+    if(err){ return next(err); }
+    
+    res.json(post);
+  });
+});
+
+/* GET a single post */
 
 module.exports = router;
